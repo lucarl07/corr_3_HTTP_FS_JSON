ba38bbf4a8a566bf98da7c6598aeedb260d39347
lucarl07 <junitinho2007@gmail.com>
2024-05-08 09:50:54 -0300

    Create readRecipeData module

---
+++ readRecipeData.js
@@ -0,0 +1,21 @@
+/** readRecipeData | by lucarl07 **/
+
+// Native modules:
+import fs from "node:fs";
+
+// Function for reading recipe data:
+const readRecipeData = (callback) => {
+  fs.readFile('./recipes.json', 'utf8', (err, data) => {
+    if (err) callback(err)
+
+    try {
+      const recipes = JSON.parse(data)
+      callback(null, recipes);
+    } catch (error) {
+      callback(error)
+    }
+  });
+}
+
+// Exporting the function as a module:
+export default readRecipeData;
